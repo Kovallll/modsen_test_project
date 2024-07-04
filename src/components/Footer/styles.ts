90c176e9daf10a3730e90c6f2f34518a790c0541
KoVaL <kovallll.e@gmail.com>
2024-07-04 16:49:22 +0300

    make footer

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+  display: flex;
+  justify-content: center;
+  padding: 16px 40px;
+  @media (max-width: 768px) {
+    padding: 8px 30px;
+  }
+  @media (max-width: 425px) {
+    padding: 4px 20px;
+  }
+`;
+
+export const Content = styled.div`
+  max-width: 1280px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const FooterImage = styled.img`
+  width: 200px;
+  height: 80px;
+  @media (max-width: 768px) {
+    width: 140px;
+    height: 42px;
+  }
+  @media (max-width: 425px) {
+    width: 120px;
+    height: 36px;
+  }
+`;
